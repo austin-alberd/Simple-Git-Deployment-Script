0627cfb453813d905b1f01a89e2bd94b712bfa7d
aalberd <austinalberd2005@gmail.com>
2025-04-13 15:32:08 -0500

    adds working edition without CLI arguements

--- main.sh
+++ main.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+
+git clone https://github.com/austin-alberd/Dev_Portfolio clonedRepo
+echo "✅ Successfully Cloned Repo"
+
+rm clonedRepo/.gitattributes
+rm clonedRepo/ .git
+echo "✅ Successfully Prepared Repository Folder"
+
+rm /var/www/html/*
+mv clonedRepo/* /var/www/html
+echo "✅ Moved Repo Folder Contents to Target Directory"
+
+rm -r clonedRepo
+echo "✅ Opperation Finished Successfully "
+
