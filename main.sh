c0071f041a938e51b560341c373d8ff5e2f28694
aalberd <austinalberd2005@gmail.com>
2025-04-13 15:47:27 -0500

    adds documentation

--- main.sh
+++ main.sh
@@ -2,17 +2,21 @@
 
 #main.sh [remote-repo-address] [target-directory]
 
+#Clone the remote repo
 git clone $1 clonedRepo
 echo "✅ Successfully Cloned Repo"
 
+#remove all of the git stuff from the repo before we move it over
 rm clonedRepo/.gitattributes
 rm clonedRepo/ .git
 echo "✅ Successfully Prepared Repository Folder"
 
+#Move the new contents over
 rm $2/*
 mv clonedRepo/* $2
 echo "✅ Moved Repo Folder Contents to Target Directory"
 
+#remove the cloned repo folder which gets everything cleaned up and ready for the next run
 rm -r clonedRepo
 echo "✅ Opperation Finished Successfully "
 
